b76401b49fc8cb0e5b8ea40928d5a7f16afe039d
AKKAMGARI VENU <bt19cse013@nituk.ac.in>
2023-11-09 15:34:55 +0530

    base

--- server.js
+++ server.js
@@ -12,18 +12,22 @@ const app = express();
 dotenv.config();
 connectDB();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
 
 app.use(express.json());
 app.use(cookieParser());
 
 //----routes
+
+app.get("/", (req, res) => {
+  res.send("API IS RUNNING");
+});
 
 app.use("/api", product);
 app.use("/api", user);
