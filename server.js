1e26e144c4b89388505e44a08242b4e03aec9b03
AKKAMGARI VENU <bt19cse013@nituk.ac.in>
2023-11-10 17:12:55 +0530

    nferfjh,hhe

--- server.js
+++ server.js
@@ -12,18 +12,24 @@ const app = express();
 dotenv.config();
 connectDB();
 
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://meek-gaufre-5e445c.netlify.app"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://meek-gaufre-5e445c.netlify.app"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
 
-app.use(express.json());
-app.use(cookieParser());
+// app.use(express.json());
+// app.use(cookieParser());
+
+app.options("/*", (req, res) => {
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.status(200).send();
+});
 
 //----routes
 
