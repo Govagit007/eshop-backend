e9fcc17fb1f1a276ef5d4e1786e832688ea02e82
AKKAMGARI VENU <bt19cse013@nituk.ac.in>
2023-11-10 16:15:43 +0530

    jenf4

--- server.js
+++ server.js
@@ -13,13 +13,12 @@ dotenv.config();
 connectDB();
 
 app.use((req, res, next) => {
-  res.setHeader(
+  res.header(
     "Access-Control-Allow-Origin",
-    "https://meek-gaufre-5e445c.netlify.app/"
+    "https://meek-gaufre-5e445c.netlify.app"
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
