d7a4c2460f9d7dcd98f3e13971fc0f1c271d09a2
AKKAMGARI VENU <bt19cse013@nituk.ac.in>
2023-11-10 17:34:43 +0530

    nfrefwrigvhjme

--- server.js
+++ server.js
@@ -6,38 +6,29 @@ const product = require("./routes/productRoutes");
 const user = require("./routes/userRoutes");
 const order = require("./routes/orderRoutes");
 const errorMiddleware = require("./middleware/error");
+const cors = require("cors");
 
 const app = express();
 
 dotenv.config();
 connectDB();
 
-// app.use((req, res, next) => {
-//   res.header(
-//     "Access-Control-Allow-Origin",
-//     "https://meek-gaufre-5e445c.netlify.app"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.header("Access-Control-Allow-Headers", "Content-Type");
-//   next();
-// });
+// Use cors middleware
+app.use(
+  cors({
+    origin: "https://meek-gaufre-5e445c.netlify.app", // Replace with your frontend origin
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 
-// app.use(express.json());
-// app.use(cookieParser());
+app.use(express.json());
+app.use(cookieParser());
 
-app.options("/*", (req, res) => {
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://meek-gaufre-5e445c.netlify.app"
-  );
-
-  res.header("Access-Control-Allow-Credentials", true);
-  res.status(200).send();
-});
+// Handle preflight requests
+app.options("/*", cors());
 
 //----routes
-
 app.get("/", (req, res) => {
   res.send("API IS RUNNING");
 });
@@ -50,4 +41,6 @@ app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`API is Running On http://localhost:${PORT} `));
+app.listen(PORT, () =>
+  console.log(`API is Running On http://localhost:${PORT}`)
+);
